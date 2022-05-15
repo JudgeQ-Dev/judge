0260af3c284b14b809c983904f8b71c37d5b67a0
Dup4 <lyuzhi.pan@gmail.com>
2022-05-15 20:09:44 +0800

    feat: mount-tmpfs

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -10,7 +10,22 @@ if [ ! -f "/root/config/config.yaml" ]; then
     cp /root/judge-core/config-example.yaml /root/config/config.yaml
 fi
 
+mount_tmpfs() {
+    path="${1}"
+    size="${2}"
+
+    if [ -z "${size}" ]; then
+        size=512M
+    fi
+
+    findmnt "${path}" -t tmpfs || mount -t tmpfs -o size="${size}" tmpfs "${path}"
+}
+
 mount --bind /opt/rootfs /opt/rootfs
+
+if [ -n "${MOUNT_COMMAND}" ]; then
+    "${MOUNT_COMMAND}"
+fi
 
 if [ X"${1}" = X"primary" ]; then
     cd /root/judge-core
